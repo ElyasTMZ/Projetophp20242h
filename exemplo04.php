e3d97d33eb5f00bc186b5b14cac3964ea6c2fa4c
elyastmz <elyastmz@outlook.com>
2024-03-11 19:45:55 -0300

    exemplo04

---
+++ exemplo04.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$valor1 = 50;
+	$valor2 = 50;
+	$soma = $valor1 + $valor2;
+
+	echo "a soma de $valor1 mais $valor2 é $soma <br />";
+	echo 'A soma de ' . $valor1 . ' mais ' . $valor2 . ' é ' . $soma .  '<br />';
+
+ ?>
