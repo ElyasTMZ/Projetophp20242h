44b70ab46df7ed0802df6fb43764badf10fe8e31
elyastmz <elyastmz@outlook.com>
2024-03-20 22:09:41 -0300

    cirando funções

---
+++ calculadora.php
@@ -0,0 +1,30 @@
+<?php
+
+  $num1=11;
+  $num2=11;
+
+
+  function soma($num1, $num2){
+		return ($num1 + $num2);
+  }
+
+  function sub($num1, $num2){
+                return ($num1 - $num2);
+  }
+
+  function div($num1, $num2){
+                return ($num1 / $num2);
+  }
+
+  function multi($num1, $num2){
+                return ($num1 * $num2);
+  
+  }
+
+
+
+
+
+
+
+?>
---
+++ exemplo09.php
@@ -0,0 +1,13 @@
+<?php
+	function soma($num1, $num2){
+		return ($num1 + $num2);
+	}
+
+	echo soma(10, 20);
+
+
+
+
+
+
+?>
